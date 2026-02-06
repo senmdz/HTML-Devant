471b4b39f0bebf86c7782b41f73bc96dc33e6ff8
senmdz <pedrohenriquedesenamedeiros@gmail.com>
2026-02-06 07:56:46 -0300

    Primeiro commit

---
+++ js/indicador-aba.js
@@ -0,0 +1,14 @@
+
+const indicador = document.querySelector(".indicador");
+const ativo = document.querySelector(".nav-link.ativo");
+
+function moverIndicador() {
+    const rect = ativo.getBoundingClientRect();
+    const navRect = ativo.closest(".nav").getBoundingClientRect();
+
+    indicador.style.width = rect.width + "px";
+    indicador.style.left = rect.left - navRect.left + 5 + "px";
+}
+
+moverIndicador();
+window.addEventListener("resize", moverIndicador);
