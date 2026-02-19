fd46cc06740c174b1eb83c034f69c10cacde4a64
Pedro Sena <pedrohenriquedesenamedeiros@gmail.com>
2026-02-19 17:08:52 -0300

    Interface do professor finalizado

--- js/desempenho.js
+++ js/desempenho.js
@@ -1,12 +1,12 @@
-function abrirPopUp() {
+function abrirPopUpDes() {
     document.getElementById("popup-desempenho").classList.remove("hidden");
 }
-function fecharPopUp() {
+function fecharPopUpDes() {
     document.getElementById("popup-desempenho").classList.add("hidden");
 }
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "Escape") {
-        fecharPopUp();
+        fecharPopUpDes();
     }
 });
--- js/observacao.js
+++ js/observacao.js
@@ -1,12 +1,12 @@
 function abrirPopUpObs() {
     document.getElementById("popup-observação").classList.remove("hidden");
 }
-function fecharPopUp() {
+function fecharPopUpObs() {
     document.getElementById("popup-observação").classList.add("hidden");
 }
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "Escape") {
-        fecharPopUp();
+        fecharPopUpObs();
     }
 });
